10ead253af9e8291b12d63124b89945a93f39143
Trisha Goodwin <trishabromero@gmail.com>
2025-02-26 19:22:03 -0600

    updated code for list and sum

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
  * @returns {number} the length of the array
  */
 function getLength(numbers) {
-  // TODO
+  return numbers.length;
 }
 
 /**
@@ -45,7 +45,10 @@ function getLength(numbers) {
  * @returns {number} the sum of the numbers
  */
 function getSum(numbers) {
-  // TODO
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+} return sum;
 }
 
 /**
